f8a89315f706f7f39350ee0a863bcddfed8c4c8f
Ayoosh <>
2025-05-27 12:20:53 +0300

    update design

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,15 @@ function App() {
   return (
     <>
       <div className="bg-gray-200">
-        <div className="p-4">
-          <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-2 mt-5 justify-items-center">
+        <div className="p-4 flex items-center justify-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mt-5 justify-center w-[90%] ">
             {products.map((product) => (
               <div
                 className="p-4 border rounded-lg shadow-md flex flex-col items-center justify-center"
               >
-                <h1>{product.title}</h1>
-                <img src={product.image} />
-                <p>${product.price}</p>
+                <h1 className=" max-w-90 font-bold text-gray-700">{product.title}</h1>
+                <img className="h-90 w-90" src={product.image} />
+                <p className="text-gray-700">${product.price}</p>
               </div>
             ))}
           </div>
